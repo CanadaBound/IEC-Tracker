8634fe3ff451e6b948fa049e1c2173a197f4c056
CanadaBound <dnamyslaw98@outlook.com>
2022-01-16 10:21:00 +0000

    Main card developed

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css';
 import Card from './Components/Card';
 
@@ -6,12 +7,15 @@ import Quotes from './Components/Quotes';
 import Search from './Components/Search';
 
 function App() {
+
+  const [selectedCountry, setSelectedCountry] = useState('');
+
   return (
    <div className='App'>
       <Navigation/>
-      <Search/>
+      <Search setSelectedCountry ={setSelectedCountry}/>
       <div className='Card-Holder'>
-        <Card/>
+        <Card selectedCountry ={selectedCountry}/>
       </div>
       <Quotes/>
    </div>
